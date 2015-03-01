8bd6a244a2ae10b4226a6acbf95266b42bda9f52
wuchangming <wuchangmingnice@163.com>
2015-03-01 14:27:23 +0800

    create browserType

---
+++ src/main/java/com/woqu/wap/responsive/browser/BrowserType.java
@@ -0,0 +1,25 @@
+package com.woqu.wap.responsive.browser;
+/**
+ * Enumeration for the type of browser that has been resolved
+ * @author WuChangming
+ *
+ */
+public enum BrowserType {
+	
+	/**
+	 * Represents a normal browser.
+	 */
+	NORMAL,
+	/**
+	 * Represents a iOS APP browser.
+	 */
+	WOQU_IOS_APP,
+	/**
+	 * Represents a android APP browser.
+	 */
+	WOQU_ANDROID_APP,
+	/**
+	 * Represents a WeiXin browser. 
+	 */
+	WEIXIN
+}
